ce93876cfc6dfbaa370c4b4c4f02d926e1c3aa3e
alex DS <32990752+alex0360@users.noreply.github.com>
2020-01-29 12:33:51 -0400

    version 0.1
    Primera version de comunicacion TCP/IP

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace SocketERTramas
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Comunicacion01 con = new Comunicacion01("10.0.0.2", 2018, "10.0.0.3", 7060);
+
+            Console.WriteLine("{0}->{1}", con.ConnectLocal, con.ConnectRemota);
+            con.EnviarRecibirPOS();
+
+            Console.ReadKey();
+        }
+    }
+}
